5bede70fdbf7ea10f219b2848571fa76cdf47bf8
Shahin Sorkh <sorkh.shahin@hotmail.com>
2018-09-07 02:37:05 +0430

    install font-awesome 4.7

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ require_once 'includes/index.php';
     <meta charset="UTF-8">
     <title>MY BOOK SHELL</title>
     <link rel="stylesheet" href="/assets/css/normalize.css">
+    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="/assets/css/font-awesome.min.css">
     <link rel="icon" href="/favicon.ico">
 </head>
 <body>
