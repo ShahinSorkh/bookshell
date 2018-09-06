ebaad46ab964d7205703c623c1fc4de4381a9619
Shahin Sorkh <sorkh.shahin@hotmail.com>
2018-09-07 03:45:10 +0430

    create login/register page

--- template/head.php
+++ template/head.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../includes/index.php';
+$current_page = $current_page ?? 'home';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,8 +20,9 @@ require_once __DIR__ . '/../includes/index.php';
         <nav>
             <ul>
                 <li><a href="/">MY BOOK SHELL</a></li>
-                <li><a class="active" href="/"><i class="fa fa-home"></i></a></li>
-                <li><a class="" href="#">USER</a></li>
+                <li><a class="<?= $current_page === 'home' ? 'active' : ''; ?>" href="/"><i class="fa fa-home"></i></a></li>
+                <li><a class="<?= $current_page === 'login' ? 'active' : ''; ?>" href="/login.php">LOGIN</a></li>
+                <li><a class="<?= $current_page === 'user' ? 'active' : ''; ?>" href="#">USER</a></li>
             </ul>
         </nav>
         <br style="clear: both;">
