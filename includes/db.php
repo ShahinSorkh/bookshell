37d93d0fba3d7beb0a9c1e622f94ca776ba1d459
Shahin Sorkh <sorkh.shahin@hotmail.com>
2018-09-06 22:42:01 +0430

    initiate

---
+++ includes/db.php
@@ -0,0 +1,10 @@
+<?php
+
+$DB_HOST    =   '127.0.0.1';
+$DB_PORT    =   3306;
+$DB_NAME    =   'bookshell';
+$DB_USER    =   'root';
+$DB_PASS    =   '';
+
+$db = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'includes/index.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MY BOOK SHELL</title>
+    <link rel="stylesheet" href="/assets/css/normalize.css">
+    <link rel="icon" href="/favicon.ico">
+</head>
+<body>
+    <h1>HEY!</h1>
+</body>
+</html>
+
+
+<?php
+require_once 'includes/terminate.php';
+?>
+
