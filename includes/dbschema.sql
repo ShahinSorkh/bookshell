03dc84d8d94f7d33a7940c2a92feaea5f56b2470
Shahin Sorkh <sorkh.shahin@hotmail.com>
2018-09-07 04:00:59 +0430

    create table users

---
+++ includes/dbschema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    password VARCHAR(45) NOT NULL
+);
+
