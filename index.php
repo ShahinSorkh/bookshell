80103515045cf3ced1cff8e7acd000e68f4be40d
Shahin Sorkh <sorkh.shahin@hotmail.com>
2018-09-07 02:40:17 +0430

    style how books would show up in home page

--- index.php
+++ index.php
@@ -12,7 +12,99 @@ require_once 'includes/index.php';
     <link rel="icon" href="/favicon.ico">
 </head>
 <body>
-    <h1>HEY!</h1>
+    <header class="header">
+        <aside>
+            <a href="/"><img src="/assets/img/logo-white-512x512.png" alt="BOOK SHELL"></a>
+        </aside>
+        <nav>
+            <ul>
+                <li><a href="/">MY BOOK SHELL</a></li>
+                <li><a class="active" href="/"><i class="fa fa-home"></i></a></li>
+                <li><a class="" href="#">USER</a></li>
+                <li><a class="" href="#">BLOG</a></li>
+                <li><a class="" href="#">SHOP</a></li>
+            </ul>
+        </nav>
+        <br style="clear: both;">
+    </header>
+    <main class="main">
+        <section class="book-card">
+            <header class="book-head">
+                <a href="#"><h3>BOOK 1</h3></a>
+            </header>
+            <article class="book-body">
+                <img src="/assets/img/books-cover/Creative Selection.png" alt="Creative Selection">
+                <p>
+                    Lorem architecto quis sunt blanditiis cum, delectus Dicta necessitatibus dolorum sit quos tempore laboriosam! Ullam ratione unde tenetur esse obcaecati. Minus officia ex commodi modi officiis. Ipsam possimus reprehenderit reiciendis!
+                </p>
+            </article>
+            <footer class="book-foot"><a href="#">READ MORE</a></footer>
+        </section>
+        <section class="book-card">
+            <header class="book-head">
+                <a href="#"><h3>BOOK 1</h3></a>
+            </header>
+            <article class="book-body">
+                <img src="/assets/img/books-cover/Fashion Climbing.png" alt="Fashion Climbing">
+                <p>
+                    Lorem architecto quis sunt blanditiis cum, delectus Dicta necessitatibus dolorum sit quos tempore laboriosam! Ullam ratione unde tenetur esse obcaecati. Minus officia ex commodi modi officiis. Ipsam possimus reprehenderit reiciendis!
+                </p>
+            </article>
+            <footer class="book-foot"><a href="#">READ MORE</a></footer>
+        </section>
+        <section class="book-card">
+            <header class="book-head">
+                <a href="#"><h3>BOOK 1</h3></a>
+            </header>
+            <article class="book-body">
+                <img src="/assets/img/books-cover/Lake Success.png" alt="Lake Success">
+                <p>
+                    Lorem architecto quis sunt blanditiis cum, delectus Dicta necessitatibus dolorum sit quos tempore laboriosam! Ullam ratione unde tenetur esse obcaecati. Minus officia ex commodi modi officiis. Ipsam possimus reprehenderit reiciendis!
+                </p>
+            </article>
+            <footer class="book-foot"><a href="#">READ MORE</a></footer>
+        </section>
+        <section class="book-card">
+            <header class="book-head">
+                <a href="#"><h3>BOOK 1</h3></a>
+            </header>
+            <article class="book-body">
+                <img src="/assets/img/books-cover/One Perfect Kiss.png" alt="One Perfect Kiss">
+                <p>
+                    Lorem architecto quis sunt blanditiis cum, delectus Dicta necessitatibus dolorum sit quos tempore laboriosam! Ullam ratione unde tenetur esse obcaecati. Minus officia ex commodi modi officiis. Ipsam possimus reprehenderit reiciendis!
+                </p>
+            </article>
+            <footer class="book-foot"><a href="#">READ MORE</a></footer>
+        </section>
+        <section class="book-card">
+            <header class="book-head">
+                <a href="#"><h3>BOOK 1</h3></a>
+            </header>
+            <article class="book-body">
+                <img src="/assets/img/books-cover/Two Dark Reigns.png" alt="Two Dark Reigns">
+                <p>
+                    Lorem architecto quis sunt blanditiis cum, delectus Dicta necessitatibus dolorum sit quos tempore laboriosam! Ullam ratione unde tenetur esse obcaecati. Minus officia ex commodi modi officiis. Ipsam possimus reprehenderit reiciendis!
+                </p>
+            </article>
+            <footer class="book-foot"><a href="#">READ MORE</a></footer>
+        </section>
+        <br style="clear: both;">
+        <section class="book-card">
+            <header class="book-head">
+                <a href="#"><h3>BOOK 1</h3></a>
+            </header>
+            <article class="book-body">
+                <img src="/assets/img/books-cover/With Shuddering Fall.png" alt="With Shuddering Fall">
+                <p>
+                    Lorem architecto quis sunt blanditiis cum, delectus Dicta necessitatibus dolorum sit quos tempore laboriosam! Ullam ratione unde tenetur esse obcaecati. Minus officia ex commodi modi officiis. Ipsam possimus reprehenderit reiciendis!
+                </p>
+            </article>
+            <footer class="book-foot"><a href="#">READ MORE</a></footer>
+        </section>
+        <br style="clear: both;">
+    </main>
+    <footer>
+    </footer>
 </body>
 </html>
 
