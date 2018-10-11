4095e3a5cf4feaa0c8b34513bb9f8a99fce7da74
Shahin Sorkh <sorkh.shahin@hotmail.com>
2018-10-11 17:49:02 +0330

    add books table

--- includes/dbschema.sql
+++ includes/dbschema.sql
@@ -12,3 +12,12 @@ CREATE TABLE users (
 
 INSERT INTO users (username, email, password, role) VALUES ('admin', 'admin@local.host', '$2y$10$HyxQDw4rEpPEAL/Ua9IJku.85jdmredVRAOiorLIrtxMBVpK2Wf7K', 'admin');
 
+CREATE TABLE IF NOT EXISTS books (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    cover VARCHAR(60) NOT NULL,
+    price INT UNSIGNED NOT NULL,
+    description TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
