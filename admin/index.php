8cc678ef13c9881493997c71800346e190c23e17
Shahin Sorkh <sorkh.shahin@hotmail.com>
2018-10-11 17:17:24 +0330

    add admin/new-book page

--- admin/index.php
+++ admin/index.php
@@ -7,9 +7,15 @@ include_once __DIR__ . '/../template/head.php';
 $page = $_GET['page'] ?? null;
 if ($page && is_file(__DIR__ . "/{$page}.php")) {
     include_once __DIR__ . "/{$page}.php";
-} else {
-    redirect('/index.php', 'page not found');
-}
+} else { ?>
+<main class="main">
+
+<ul>
+    <li><a href="<?= $_SERVER['PHP_SELF']; ?>?page=new-book">Create new book</a></li>
+</ul>
+
+</main>
+<?php }
 
 include_once __DIR__ . '/../template/foot.php';
 
