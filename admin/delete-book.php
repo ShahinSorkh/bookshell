ebc3479e3e4444e73a6493684790b8c2c69736c8
Shahin Sorkh <sorkh.shahin@hotmail.com>
2018-10-11 19:50:23 +0330

    delete books

---
+++ admin/delete-book.php
@@ -0,0 +1,12 @@
+<?php
+$book_id = intval($_GET['id']);
+$result = mysqli_query($db, "select * from books where id=$book_id limit 1");
+if (!$result) redirect('/admin/index.php?page=list-books', mysqli_error($db), 'danger');
+if (mysqli_num_rows($result) < 1) redirect('/admin/index.php?page=list-books', 'book not found', 'danger');
+
+$book = mysqli_fetch_assoc($result);
+
+if (unlink(ROOT_DIR . $book['cover']) && mysqli_query($db, "delete from books where id=$book[id] limit 1"))
+    redirect('/admin/index.php?page=list-books', 'book deleted', 'success');
+else redirect('/admin/index.php?page=list-books', 'could not delete: ' . mysqli_error($db), 'danger');
+
